f064339432440621c01ac1aede77162e22721ad9
nshumoogum <nathan.shumoogum@methods.co.uk>
2017-07-14 13:38:11 +0100

    Make consumer group configurable

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	consumerGroup, err := kafka.NewConsumerGroup(cfg.Brokers, cfg.InputFileAvailableTopic, "input-file-available", cfg.InputFileAvailableOffset)
+	consumerGroup, err := kafka.NewConsumerGroup(cfg.Brokers, cfg.InputFileAvailableTopic, cfg.InputFileAvailableGroup, cfg.InputFileAvailableOffset)
 	if err != nil {
 		log.ErrorC("could not obtain consumer", err, nil)
 		os.Exit(1)
