2d53fbcfc0ffce9f6cd411948717140cead298a7
Carl Hembrough <carl@bitwaves.co.uk>
2017-09-04 16:19:41 +0100

    Migrate config to envconfig allowing slice values to be parsed.

--- config/config.go
+++ config/config.go
@@ -1,19 +1,19 @@
 package config
 
-import "github.com/ian-kent/gofigure"
+import "github.com/kelseyhightower/envconfig"
 
 // Config is the filing resource handler config
 type Config struct {
-	AWSRegion                string   `env:"AWS_REGION" flag:"aws-region" flagDesc:"The AWS region to use"`
-	BindAddr                 string   `env:"BIND_ADDR" flag:"bind-addr" flagDesc:"The port to bind to"`
-	Brokers                  []string `env:"KAFKA_ADDR" flag:"kafka-addr" flagDesc:"The Kafka broker addresses"`
-	DimensionsExtractedTopic string   `env:"DIMENSIONS_EXTRACTED_TOPIC" flag:"dimensions-extracted-topic" flagDesc:"The Kafka topic to write dimension messages to"`
-	ImportAPIURL             string   `env:"IMPORT_API_URL" flag:"import-api-url" flagDesc:"The import api url"`
-	ImportAPIAuthToken       string   `env:"IMPORT_AUTH_TOKEN" flag:"import-auth-token" flagDesc:"Authentication token for access to import API"`
-	InputFileAvailableGroup  string   `env:"INPUT_FILE_AVAILABLE_GROUP" flag:"input-file-available-group" flagDesc:"The Kafka consumer group to consume file messages from"`
-	InputFileAvailableTopic  string   `env:"INPUT_FILE_AVAILABLE_TOPIC" flag:"input-file-available-topic" flagDesc:"The Kafka topic to consume file messages from"`
-	KafkaMaxBytes            string   `env:"KAFKA_MAX_BYTES" flag:"kafka-max-bytes" flagDesc:"The maximum permitted size of a message. Should be set equal to or smaller than the broker's 'message.max.bytes'"`
-	MaxRetries               int      `env:"REQUEST_MAX_RETRIES" flag:"request-max-retries" flagDesc:"The maximum number of attempts for a single http request due to external service failure"`
+	AWSRegion                string   `envconfig:"AWS_REGION"`
+	BindAddr                 string   `envconfig:"BIND_ADDR"`
+	Brokers                  []string `envconfig:"KAFKA_ADDR"`
+	DimensionsExtractedTopic string   `envconfig:"DIMENSIONS_EXTRACTED_TOPIC"`
+	ImportAPIURL             string   `envconfig:"IMPORT_API_URL"`
+	ImportAPIAuthToken       string   `envconfig:"IMPORT_AUTH_TOKEN"`
+	InputFileAvailableGroup  string   `envconfig:"INPUT_FILE_AVAILABLE_GROUP"`
+	InputFileAvailableTopic  string   `envconfig:"INPUT_FILE_AVAILABLE_TOPIC"`
+	KafkaMaxBytes            string   `envconfig:"KAFKA_MAX_BYTES"`
+	MaxRetries               int      `envconfig:"REQUEST_MAX_RETRIES"`
 }
 
 var cfg *Config
@@ -37,9 +37,5 @@ func Get() (*Config, error) {
 		ImportAPIAuthToken:       "FD0108EA-825D-411C-9B1D-41EF7727F465",
 	}
 
-	if err := gofigure.Gofigure(cfg); err != nil {
-		return cfg, err
-	}
-
-	return cfg, nil
+	return cfg, envconfig.Process("", cfg)
 }
