df8bca13545b16a7cf5449666159457b93faaddf
David Subiros <subiros.david@gmail.com>
2020-02-25 17:08:17 +0000

    Updated ci scripts and ymls

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,9 +2,7 @@
 
 cwd=$(pwd)
 
-export GOPATH=$cwd/go
-
-pushd $GOPATH/src/github.com/ONSdigital/dp-dimension-extractor
+pushd $cwd/dp-dimension-extractor
   make build && mv build/$(go env GOOS)-$(go env GOARCH)/* $cwd/build
   cp Dockerfile.concourse $cwd/build
 popd
--- ci/scripts/unit.sh
+++ ci/scripts/unit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
-export GOPATH=$(pwd)/go
+cwd=$(pwd)
 
-pushd $GOPATH/src/github.com/ONSdigital/dp-dimension-extractor
+pushd $cwd/dp-dimension-extractor
   make test
 popd
