13be30efe1a4ea17b225839c4f02245f4aba1525
Justin Pattarukuzhyil Jose <justin.p.jose@ons.gov.uk>
2021-09-15 16:37:06 +0300

    update validation on kafka client key and cert

--- config/validation.go
+++ config/validation.go
@@ -19,11 +19,12 @@ func (kafkaConfig KafkaConfig) validateKafkaValues() []string {
 		errs = append(errs, "KAFKA_SEC_PROTO has invalid value")
 	}
 
-	// isKafkaClientCertSet xor isKafkaClientKeySet
 	isKafkaClientCertSet := len(kafkaConfig.SecClientCert) != 0
 	isKafkaClientKeySet := len(kafkaConfig.SecClientKey) != 0
-	if (isKafkaClientCertSet || isKafkaClientKeySet) && !(isKafkaClientCertSet && isKafkaClientKeySet) {
-		errs = append(errs, "only one of KAFKA_SEC_CLIENT_CERT or KAFKA_SEC_CLIENT_KEY has been set - requires both")
+	if isKafkaClientKeySet && !isKafkaClientCertSet {
+		errs = append(errs, "no KAFKA_SEC_CLIENT_CERT given but got KAFKA_SEC_CLIENT_KEY")
+	} else if isKafkaClientCertSet && !isKafkaClientKeySet {
+		errs = append(errs, "no KAFKA_SEC_CLIENT_KEY given but got KAFKA_SEC_CLIENT_CERT")
 	}
 
 	return errs
